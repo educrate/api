18dbdc2e63f5b6a7943dc3f39733fa25249577e7
christianampe <christianampe.work@gmail.com>
2019-01-07 14:46:03 -0800

    updated logic to contain user and not users

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,5 @@
 const college = require('./college/college.service.js');
-const users = require('./users/users.service.js');
+const user = require('./user/user.service.js');
 const course = require('./course/course.service.js');
 const instructor = require('./instructor/instructor.service.js');
 const job = require('./job/job.service.js');
@@ -11,7 +11,7 @@ const tutor = require('./tutor/tutor.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(college);
-  app.configure(users);
+  app.configure(user);
   app.configure(course);
   app.configure(instructor);
   app.configure(instructor);
