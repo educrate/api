1afc04c6fbc521574750a1854a5f9084bd43b3eb
christianampe <christianampe.work@gmail.com>
2019-01-07 14:16:07 -0800

    added user params

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -11,17 +11,20 @@ module.exports = function (app) {
       defaultValue: DataTypes.UUIDV1, 
       primaryKey: true 
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
     },
     password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    id: {
       type: DataTypes.STRING,
       allowNull: false
     }
